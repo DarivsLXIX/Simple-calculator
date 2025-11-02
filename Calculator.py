8b0ac5ea45d63a892299e2e1b073207e1638fbd1
DarivsLXIX <costindarius027@gmail.com>
2025-11-02 16:12:42 +0200

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,21 +1,15 @@
-num1 = input('Enter a number:')
-num2 = input('Enter another number:')
-print('Enter your operation:')
-op = input()
 
-try:
-    if op == '+':
-        print(int(num1) + int(num2))
-    elif op == '-':
-        print(int(num1) - int(num2))
-    elif op == '*':
-        print(int(num1) * int(num2))
-    elif op == '/':
-        print(int(num1) / int(num2))
-    else:
-        print('Invalid operation.')
-except ZeroDivisionError:
-    print("Error: You cannot divide by zero.")
-except ValueError:
-    print("Error: Please enter valid numbers.")
-input("Press Enter to exit...")
+while True:
+    num1 = float(input('Enter a number:'))
+    operator = input('Enter a operator (+, -, *, /) or q to quit:')
+    if operator == 'q':
+        break
+    num2 = float(input('Enter another number:'))
+    if operator == '+':
+        print(num1 + num2)
+    elif operator == '-':
+        print(num1 - num2)
+    elif operator == '*':
+        print(num1 * num2)
+    elif operator == '/':
+        print(num1 / num2)
